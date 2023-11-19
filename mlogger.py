106e5e204e019e0018169da56a58b4b368b43b82
Michael Kiessling <mkiessling89@googlemail.com>
2023-11-19 14:39:30 +0100

    use 'os.makedirs' instead of simple 'os.mkdir' to create log dir structure

--- mlogger.py
+++ mlogger.py
@@ -49,15 +49,8 @@ Data can be stored as plain text or in JSON format """
         self.time_stamp=(str(self.t[1])+"-"+str(self.t[2])+"-"+\
         str(self.t[3])+"-"+str(self.t[4]))                                                                             
         logging.info("creating sub directory"+str(self.time_stamp))
-        try:
-            os.stat(self.log_dir)
-        except:
-            os.mkdir(self.log_dir) 
         self.log_sub_dir=self.log_dir+"/"+self.time_stamp 
-        try:
-            os.stat(self.log_sub_dir)
-        except:
-            os.mkdir(self.log_sub_dir)
+        os.makedirs(self.log_sub_dir, exist_ok=True)
         return(self.log_sub_dir)
 
     def get_log_name(self,log_dir,count):
